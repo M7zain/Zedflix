6ad3ed51c00a3e4247488cefc76c457aed7e7b05
zain <99236670+M7zain@users.noreply.github.com>
2023-07-20 02:27:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import SearchIcon from'./search.svg';
 import MovieCard from './MovieCard';
-// API key  
-//2a911f8a
 
-const API_URL ='http://www.omdbapi.com?apikey=2a911f8a';
-
-const movie1 ={
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-};
-
+// API key/URL
+const API_URL ='http://www.omdbapi.com?apikey=YourAPIKey';
 
 
 const App = ()=>{
